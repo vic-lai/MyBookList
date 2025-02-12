5bc4155f9b376fc3713a48f6e34f2c4aa80c7755
Victor Lai <laivictor80@gmail.com>
2025-02-12 12:25:43 -0800

    setup frontend

---
+++ frontend/src/components/home/Home.jsx
@@ -0,0 +1,11 @@
+import { Typography, Box } from '@mui/material';
+
+const Home = () => {
+    return (
+        <Box>   
+            <Typography sx={{fontSize:'30px'}}>MyBookList</Typography>
+        </Box>
+    );
+}
+ 
+export default Home;
